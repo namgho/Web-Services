09c13d9702a16e126ad4b023845ccf126d255927
nam <nam@NAM-PC>
2018-11-02 11:26:28 +1100

    to github

--- PlantPlacesWebService/Hello World/Controllers/HomeController.cs
+++ PlantPlacesWebService/Hello World/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Hello_World.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello World.";
 
             return View();
         }
